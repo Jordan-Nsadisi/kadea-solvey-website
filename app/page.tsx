fb384dd9816be894f4fe2868b3c7da6e39c6a578
Jordan-Nsadisi <astro17nsadisi@gmail.com>
2026-07-09 21:29:16 +0100

    add sessions section in app component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Institution from '@/components/sections/Institution'
 import Navbar from '@/components/sections/Navbar'
 import Partners from '@/components/sections/Partners'
 import Programme from '@/components/sections/Programme'
+import Sessions from '@/components/sections/Sessions'
 import React from 'react'
 
 export default function Home() {
@@ -14,7 +15,7 @@ export default function Home() {
         <Partners />
         <Institution />
         <Programme />
-        {/* <Sessions /> */}
+        <Sessions />
         {/* <Admission /> */}
       </main>
       {/* <Footer /> */}
