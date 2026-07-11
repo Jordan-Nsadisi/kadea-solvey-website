34b11da68bc2209376075095ef4e4b921c883ddb
Jordan-Nsadisi <astro17nsadisi@gmail.com>
2026-07-11 14:25:36 +0100

    refactor: remove commented-out Partners component from main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
-        {/* <Partners /> */}
         <NationalContext />
         <Institution />
         <Programme />
