0b45ff6aaac3139f1279238aa65becfc0a244f8e
Jordan-Nsadisi <astro17nsadisi@gmail.com>
2026-07-12 02:35:37 +0100

    style: remove sm:w-28 responsive class from header logo

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
                             alt="ULB-Solvay Lifelong Learning"
                             width={220}
                             height={46}
-                            className="w-30 sm:w-28 md:w-74"
+                            className="w-30 md:w-74"
                         />
                         <div className="w-px h-4 md:h-6 bg-gray-200" />
                         <ImageWithFallback
