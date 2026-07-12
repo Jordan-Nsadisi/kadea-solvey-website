b81e6f5daeccda55199ad55cde00d9b7d5152603
Jordan-Nsadisi <astro17nsadisi@gmail.com>
2026-07-12 03:10:03 +0100

    refactor: move font size styling from header button to base button component

--- components/ui/atoms/button.tsx
+++ components/ui/atoms/button.tsx
@@ -49,7 +49,7 @@ function Button({
     return (
         <Comp
             data-slot="button"
-            className={cn(buttonVariants({ variant, size, className }))}
+            className={"text-sm md:text-[15px]" + cn(buttonVariants({ variant, size, className }))}
             {...props}
         />
     );
